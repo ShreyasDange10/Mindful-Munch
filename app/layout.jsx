9d32c8c2d3294a5f6162cd958d08041b68b28d86
unknown <shreyasdange2408@gmail.com>
2023-12-15 10:18:31 +0530

    Thirdtimecodepush

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,13 @@
 import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
   title: "Mindful Munch",
   description: ""
 }
 
-const RootLayout = () => {
+const RootLayout = ( {children} ) => {
   return (
     <html lang = "en">
       <body>
@@ -14,6 +16,7 @@ const RootLayout = () => {
         </div>
 
         <main className='app'>
+          <Nav/>
           {children}
         </main>
       </body>
